33d7f28ceea3fcc9c6bdfafb793361c712670121
Lucas Ferreira <lucas.ferreira0511@hotmail.com>
2022-10-23 20:59:52 +0200

    add graph from g6

--- src/App.js
+++ src/App.js
@@ -2,14 +2,20 @@ import './App.scss';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import Body from './components/Body';
+import Graphs from './components/Graphs';
 
 function App() {
   return (
     <div className="App">
+
         {/* navbar */}
         <Navbar />
+
         {/* body */}
         <Body />
+
+        {/* Graphs */}
+        <Graphs />
         {/* footer */}
         <Footer />
     </div>
